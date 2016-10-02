5b3c15ae97858419a19002cec91f241a48b96ea8
Hadrien David <hadrien@ectobal.com>
2016-10-02 09:42:17 -0400

    Fix packaging
    Remove empty requirements file.
    Move test file in top directory.
    Add MANIFEST.in to include VERSION file in sdist.

--- setup.py
+++ setup.py
@@ -1,9 +1,6 @@
 from setuptools import setup
 
-with open('requirements.txt', 'r') as f:
-    requirements = f.read()
-
-with open('requirements-tests.txt', 'r') as f:
+with open('requirements-tests.txt') as f:
     test_requirements = f.read()
 
 with open('VERSION') as f:
@@ -15,6 +12,5 @@ setup(
     name='aws_lambda_logging',
     description='Nanolib to enhance logging in aws lambda',
     py_modules=['aws_lambda_logging'],
-    install_requires=requirements,
     tests_require=test_requirements,
 )
