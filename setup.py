ff902b9dcf4fc4698ba44a0461e5f04bb3b698b7
Hadrien David <hadrien@ectobal.com>
2016-10-03 10:10:06 -0400

    Add long description in setup

--- setup.py
+++ setup.py
@@ -1,16 +1,23 @@
+from os import path
 from setuptools import setup
 
-with open('requirements-tests.txt') as f:
+here = path.abspath(path.dirname(__file__))
+
+
+with open(path.join(here, 'README.rst')) as f:
+    long_description = f.read()
+
+with open(path.join(here, 'requirements-tests.txt')) as f:
     test_requirements = f.read()
 
-with open('VERSION') as f:
+with open(path.join(here, 'VERSION')) as f:
     version = f.read().strip()
-
 
 setup(
     version=version,
     name='aws_lambda_logging',
     description='Nanolib to enhance logging in aws lambda',
+    long_description=long_description,
     py_modules=['aws_lambda_logging'],
     tests_require=test_requirements,
 )
